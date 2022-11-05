a6e7d02c4c53d59eabb6678332f59ff5f751699e
Srinivasan Ravi <srinivas2433@gmail.com>
2022-11-05 19:45:38 +0530

    Initial Commit

---
+++ JMSIntegration/src/test/java/org/srini/jeesamples/JMSIntegration/JmsIntegrationApplicationTests.java
@@ -0,0 +1,13 @@
+package org.srini.jeesamples.JMSIntegration;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JmsIntegrationApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
